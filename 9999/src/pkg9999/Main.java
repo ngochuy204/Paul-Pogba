23956b076272c4bc37ad1e3f158aa801c101400b
ngochuy204 <ngochuy204@gmail.com>
2018-08-18 08:22:23 +0700

    hghgh

---
+++ 9999/src/pkg9999/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg9999;
+
+/**
+ *
+ * @author Dell
+ */
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
